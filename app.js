567608d4dce7b5ccb30340cdc3d85d3c266f3915
Yusuf Büyüktorun <ybuyuktoruntest@gmail.com>
2022-11-04 17:40:22 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//3.party module
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('INDEX SAYFASI');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
