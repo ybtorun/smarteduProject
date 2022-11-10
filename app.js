c11f56e604995a6667e6936bbb218023ed8a3be3
Yusuf Büyüktorun <ybuyuktoruntest@gmail.com>
2022-11-10 15:00:56 +0300

    version 2

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 //3.party module
 const express = require('express');
 
+// const ejs = require('ejs');
+
+
 const app = express();
 
+//Template engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//routes
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
+  res.status(200).render('index', {
+    page_name: "index"
+  });
 });
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: "about"
+  });
+});
+
+
 
 const port = 3000;
 app.listen(port, () => {
